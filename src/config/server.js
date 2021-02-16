3e5d1899379eaff14b0eff2895b061c4591655b0
romulo lima <dhb.tecnologia@gmail.com>
2021-02-16 14:13:15 -0300

    Aplicacao Back-end utilizando banco de dados nao relacional

---
+++ src/config/server.js
@@ -0,0 +1,20 @@
+const port = 3003
+
+const express = require('express');
+
+const bodyParser = require('body-parser')
+
+const server = express()
+const allowCors = require('./cors')
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+
+server.listen(port, function() {
+    console.log(`🚀Backend is running on port ${port}.`)
+})
+
+module.exports = server
+
